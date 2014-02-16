9462d0eb7cc2831129e1249339645a80989be0f9
Chandra Sekar S <chandru.in@gmail.com>
2014-02-16 12:49:25 +0530

    Basic UI to add slides

---
+++ rpresent.go
@@ -0,0 +1,64 @@
+// Copyright 2014, Chandra Sekar S.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the README.md file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var (
+	httpAddr  = flag.String("http", ":8080", "HTTP address to listen on")
+	slidesDir = flag.String("d", "slides", "Directory to store slides in")
+	baseURL   = flag.String("b", "http://localhost:8080", "Base URL for slides")
+)
+
+func main() {
+	flag.Parse()
+
+	if err := os.MkdirAll(*slidesDir, 0700); err != nil {
+		log.Fatalln("Failed to create slides directory:", err)
+	}
+
+	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/statics/", statics)
+
+	fmt.Println("Listening at", *httpAddr)
+	http.ListenAndServe(*httpAddr, nil)
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		if r.URL.Path == "/" {
+			uploadTmpl.Execute(w, nil)
+		}
+
+	case "POST":
+		processUpload(w, r)
+
+	default:
+		http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func statics(w http.ResponseWriter, r *http.Request) {
+	parts := strings.SplitN(r.URL.Path, "/", 3)
+
+	switch parts[2] {
+	case "slides.js":
+		w.Header()["Content-Type"] = []string{"application/javascript"}
+		w.Write([]byte(slidesJS))
+	case "styles.css":
+		w.Header()["Content-Type"] = []string{"text/css"}
+		w.Write([]byte(stylesCSS))
+	default:
+		http.NotFound(w, r)
+	}
+}
---
+++ upload.go
@@ -0,0 +1,240 @@
+// Copyright 2014, Chandra Sekar S.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the README.md file.
+
+package main
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"errors"
+	"io"
+	"log"
+	"math/rand"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+	"time"
+)
+
+const keyChars = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+var (
+	errNoSlide       = errors.New("Archive must contain at least one .slide file")
+	errTooManySlides = errors.New("Archive must contain only one .slide file")
+)
+
+var uploadTmpl = template.Must(template.New("upload").Parse(`
+<!doctype html>
+<html>
+<head>
+	<title>Remote Presentations</title>
+	<style>
+	body {
+		color: rgb(51, 51, 51);
+	}
+	</style>
+</head>
+<body>
+<h1>Add/Update Presentation</h1>
+<form action="/" method="POST" enctype="multipart/form-data">
+	<label for="slideArchive">Slide Archive (.tar.gz or .tgz):</label>
+	<input type="file" id="slideArchive" name="slideArchive">
+	<p>
+	<label for="existingId">Existing Slide ID:</label>
+	<input type="text" id="existingId" name="existingId">
+	<p>
+	<input type="submit" value="Upload">
+	<input type="reset" value="Reset">
+</form>
+</body>
+</html>
+`))
+
+var msgTmpl = template.Must(template.New("message").Parse(`
+<!doctype html>
+<html>
+<head>
+	<title>{{.title}}</title>
+	<style>
+	body {
+		color: rgb(51, 51, 51);
+	}
+	</style>
+</head>
+<body>
+<h1{{if .error}} style="color: red"{{end}}>{{.msg}}</h1>
+</body>
+</html>
+`))
+
+var shareTmpl = template.Must(template.New("share").Parse(`
+<!doctype html>
+<html>
+<head>
+	<title>Slide Uploaded</title>
+	<style>
+	body {
+		color: rgb(51, 51, 51);
+	}
+	</style>
+</head>
+<body>
+<h1>Successfully Uploaded Slide</h1>
+	This information is required for updating and presenting the slide.
+	<p>
+	<label for="slideId">Slide ID:</label>
+	<input type="text" id="slideId" readonly="readonly" value="{{.slideId}}">
+	<p>
+	<label for="presenterURL">Presenter URL:</label>
+	<input type="text" id="presenterURL" readonly="readonly" value="{{.baseURL}}/{{.slideId}}">
+	<p>
+	<script>
+		document.getElementById("presenterURL").focus();
+	</script>
+</body>
+</html>
+`))
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func processUpload(w http.ResponseWriter, r *http.Request) {
+	slideId := r.FormValue("existingId")
+	if slideId == "" {
+		slideId = generateKey()
+	}
+
+	file, _, err := r.FormFile("slideArchive")
+	if err == http.ErrMissingFile {
+		w.WriteHeader(http.StatusBadRequest)
+		msgTmpl.Execute(w, map[string]interface{}{
+			"title": "Missing File",
+			"msg":   "No slide archive file was chosen",
+			"error": true,
+		})
+		return
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer file.Close()
+
+	if err := extractArchive(filepath.Join(*slidesDir, slideId), file); err != nil {
+
+		badFile := err == gzip.ErrChecksum || err == gzip.ErrHeader || err == tar.ErrHeader
+		badContent := err == errNoSlide || err == errTooManySlides
+
+		if badFile || badContent {
+			w.WriteHeader(http.StatusBadRequest)
+			msgTmpl.Execute(w, map[string]interface{}{
+				"title": "Bad Upload",
+				"msg":   err.Error(),
+				"error": true,
+			})
+			return
+		}
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	shareTmpl.Execute(w, map[string]string{
+		"slideId": slideId,
+		"baseURL": *baseURL,
+	})
+
+	return
+}
+
+func generateKey() string {
+	buf := make([]byte, 8)
+	for i := range buf {
+		buf[i] = keyChars[rand.Intn(len(keyChars))]
+	}
+
+	return string(buf)
+}
+
+func extractArchive(slideBase string, file multipart.File) (err error) {
+	if err := os.RemoveAll(slideBase); err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(slideBase, 0700); err != nil {
+		return err
+	}
+
+	defer cleanupOnFailure(slideBase, &err)
+
+	gzipReader, err := gzip.NewReader(file)
+	if err != nil {
+		return err
+	}
+	defer gzipReader.Close()
+
+	var slideCount int
+	reader := tar.NewReader(gzipReader)
+	for {
+		header, err := reader.Next()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		fileName := filepath.Join(slideBase, header.Name)
+		if header.FileInfo().IsDir() {
+			if err := os.MkdirAll(fileName, 0700); err != nil {
+				return err
+			}
+
+			continue
+		}
+
+		if strings.HasSuffix(fileName, ".slide") {
+			fileName = filepath.Join(filepath.Dir(fileName), "main.slide")
+			slideCount++
+		}
+
+		f, err := os.Create(fileName)
+		if err != nil {
+			return err
+		}
+
+		defer f.Close()
+
+		if _, err := io.Copy(f, reader); err != nil {
+			return err
+		}
+	}
+
+	if slideCount == 0 {
+		return errNoSlide
+	}
+
+	if slideCount > 1 {
+		return errTooManySlides
+	}
+
+	return nil
+}
+
+func cleanupOnFailure(slideBaseDir string, failure *error) {
+	if *failure == nil {
+		return
+	}
+
+	if err := os.RemoveAll(slideBaseDir); err != nil {
+		log.Printf("Failed to cleanup directory: %s : %s\n", slideBaseDir, err)
+	}
+}
